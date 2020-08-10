a59581a213f4ad27cccd08cf54214aa296ad3a7c
Ernesto <48799528+ernestod10@users.noreply.github.com>
2020-08-10 18:03:36 -0400

    primero

---
+++ data/Carta Solteria/solteria/helpers.js
@@ -0,0 +1,10 @@
+function now() {
+    return new Date().toLocaleDateString()
+}
+
+function nowPlus20Days() {
+    var date = new Date()
+    date.setDate(date.getDate() + 20);
+    return date.toLocaleDateString();
+}
+
