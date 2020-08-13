9dcc45c7772386b85e627b9beb5024438a7f12e5
Ernesto <48799528+ernestod10@users.noreply.github.com>
2020-08-12 20:13:40 -0400

    funciona con base de datos

--- data/Orders/orders-script/content.js
+++ data/Orders/orders-script/content.js
@@ -4,50 +4,21 @@ const https = require('https');
 // call remote http rest api
 function fetchOrders() {
     return new Promise((resolve, reject) => {
-        https.get('https://services.odata.org/V4/Northwind/Northwind.svc/Orders',
+        https.get('https://jsonplaceholder.typicode.com/users',
         (result) => {
-            var str = '';
-            result.on('data', (b) => str += b);
+            var str ;
+            result.on('data',b=> str+=b);
             result.on('error', reject);
-            result.on('end', () => resolve(JSON.parse(str).value));
+            result.on('end', () => resolve(str));
+            console.log(str);
         });
     })
 }
 
 // group the data for report
-async function prepareDataSource() {
-    const orders = await fetchOrders()
-    const ordersByShipCountry = orders.reduce((a, v) => {
-        a[v.ShipCountry] = a[v.ShipCountry] || []
-        a[v.ShipCountry].push(v)
-        return a
-    }, {})
 
-    return Object.keys(ordersByShipCountry).map((country) => {
-        const ordersInCountry = ordersByShipCountry[country]
-
-        const accumulated = {}
-
-        ordersInCountry.forEach((o) => {
-            o.OrderDate = new Date(o.OrderDate);
-            const key = o.OrderDate.getFullYear() + '/' + (o.OrderDate.getMonth() + 1);
-            accumulated[key] = accumulated[key] || {
-                value: 0,
-                orderDate: o.OrderDate
-            };
-            accumulated[key].value++;
-        });
-
-        return {
-            rows: ordersInCountry,
-            country,
-            accumulated
-        }
-
-    }).slice(0, 2)
-}
 
 // add jsreport hook which modifies the report input data
 async function beforeRender(req, res) {
-    req.data.orders = await prepareDataSource()
+    req.data.orders = await fetchOrders()
 }
