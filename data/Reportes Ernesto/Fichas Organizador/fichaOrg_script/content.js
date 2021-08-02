bb65a2b2eeffdb965d3845881dde9626925cf91e
Ernesto <48799528+ernestod10@users.noreply.github.com>
2021-08-01 22:08:10 -0400

    a

--- data/Reportes Ernesto/Fichas Organizador/fichaOrg_script/content.js
+++ data/Reportes Ernesto/Fichas Organizador/fichaOrg_script/content.js
@@ -11,21 +11,23 @@ const db = new Client({
 function beforeRender(req, res, done) {
 const idorg= req.data.idorg;
 
-
+var data1;
 
 
 
 db.connect();
 
 var query = ["Select o.nombre org, o.id_organizacion, o.fecha_fundacion, o.proposito ,c.nombre,c.apellido, c.telefono,c.email,c.doc_identidad from ec_organizacion o left join ec_organizador_contacto c on o.id_organizacion = c.ec_organizacion_id_organizacion"];
-  if (idorg!="0") query.push(" where o.id_organizacion = ('"+idorg+"') or o.nombre like ('"+idorg+"%') ");
-  query.push(" limit 1 ")
+  if ((idorg!="-1")&&(!isNaN(idorg))) query.push(" where o.id_organizacion = ('"+idorg+"')");
+  if ((idorg!="-1")&&(isNaN(idorg))) query.push(" where o.nombre like ('%"+idorg+"%') or  o.ec_lugar_id_lugar in (select id_lugar from ec_lugar where nombre_pais like ('%"+idorg+"%'))");
+  
   db.query(query.join(' '), (err, result) => {
     console.log(err);
     req.data = {
       rows: result.rows
       
     };
+  
     db.end();
     done();
   });
